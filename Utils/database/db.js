d0d7938694d66b3d78f4c49d417817b7e4585f2b
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-23 19:49:37 +0530

    check if old connection alreday exists

--- Utils/database/db.js
+++ Utils/database/db.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  if (mongoose.connections[0].readyState) {
+    return;
+  }
+
   try {
     mongoose.set("strictQuery", true);
     await mongoose.connect(process.env.DB_URL);
