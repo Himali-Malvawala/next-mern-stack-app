e669177098c6f87f4fc9cee837fc5ba7e0374905
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-23 20:20:04 +0530

    create edit-book

---
+++ pages/edit-book/[id].js
@@ -0,0 +1,11 @@
+import UpdateBookInfo from "../../components/UpdateBookInfo";
+
+const Update = () => {
+  return (
+    <div>
+      <UpdateBookInfo />
+    </div>
+  );
+};
+
+export default Update;
