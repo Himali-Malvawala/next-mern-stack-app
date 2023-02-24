418bfa8ac4b64dd8159ebe476466f7d81e6a4ed2
Himali Malvawala <himalimalvawala@gmail.com>
2023-02-24 18:29:34 +0530

    change card

--- components/Card.js
+++ components/Card.js
@@ -1,21 +1,26 @@
 import Link from "next/link";
+import moment from "moment";
 
 const Card = (props) => {
-  const book = props.data;
+  const movie = props.data;
   return (
     <div>
       <div className="max-w-xs bg-[#FFFFD0] rounded-md group">
-        <img src={book.image} alt={book.title} className="rounded-t-md" />
+        <img
+          src={movie.image}
+          alt={movie.title}
+          className="rounded-t-md h-52 object-cover object-center"
+        />
         <div className="px-3 py-3">
           <div className="sm:flex sm:justify-between sm:items-center">
             <h4 className="text-xl font-medium text-slate-800 group-hover:text-[#A555EC] group-hover:underline underline-offset-2">
-              <Link href={`/show-book/${book._id}`}>{book.title}</Link>
+              <Link href={`/show-movie/${movie._id}`}>{movie.title}</Link>
             </h4>
             <h4 className="text-base text-slate-800 mt-1 sm:mt-0">
-              {book.author}
+              {moment(movie.watched_date).format("MMM Do YYYY")}
             </h4>
           </div>
-          <p className="font-light mt-2 text-black">{book.description}</p>
+          <p className="font-light mt-2 text-black">{movie.genre}</p>
         </div>
       </div>
     </div>
