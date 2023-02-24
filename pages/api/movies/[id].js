f2a276cab145f7f721ae9b2fbcff8ee64f080afe
Himali Malvawala <himalimalvawala@gmail.com>
2023-02-24 18:26:17 +0530

    change /api/books to /api/movies

--- pages/api/movies/[id].js
+++ pages/api/movies/[id].js
@@ -1,20 +1,20 @@
-import Book from "../../../Utils/models/model";
+import Movie from "../../../Utils/models/model";
 import connectDB from "../../../Utils/database/db";
 
 export default async function showDetails(req, res) {
   if (req.method === "GET") {
     try {
       await connectDB();
-      const details = await Book.findById(req.query.id);
+      const details = await Movie.findById(req.query.id);
       res.status(200).json({ details });
     } catch (err) {
-      res.status(404).json({ nobookfound: "No Book Found" });
+      res.status(404).json({ nomoviefound: "No Movie Found" });
     }
   }
   if (req.method === "PUT") {
     try {
       await connectDB();
-      await Book.findByIdAndUpdate(req.query.id, req.body);
+      await Movie.findByIdAndUpdate(req.query.id, req.body);
       res.status(200).json({ msg: "Updated successfully" });
     } catch (err) {
       res.status(404).json({ error: "Unable to update the Database" });
@@ -24,10 +24,10 @@ export default async function showDetails(req, res) {
   if (req.method === "DELETE") {
     try {
       await connectDB();
-      await Book.findByIdAndRemove(req.query.id, req.body);
-      res.status(200).json({ mgs: "Book entry deleted successfully" });
+      await Movie.findByIdAndRemove(req.query.id, req.body);
+      res.status(200).json({ mgs: "Movie entry deleted successfully" });
     } catch (err) {
-      res.status(404).json({ error: "No such a book" });
+      res.status(404).json({ error: "No such a movie" });
     }
   }
 }
