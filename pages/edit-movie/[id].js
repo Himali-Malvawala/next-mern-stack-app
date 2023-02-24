b15ee02bb623831f9e4dc135116e19f015e6230e
Himali Malvawala <himalimalvawala@gmail.com>
2023-02-24 18:28:52 +0530

    change edit-book to edit-movie

---
+++ pages/edit-movie/[id].js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import UpdateMovieInfo from "../../components/UpdateMovieInfo";
+
+const Update = () => {
+  return (
+    <div>
+      <Head>
+        <title>Movies App</title>
+      </Head>
+      <UpdateMovieInfo />
+    </div>
+  );
+};
+
+export default Update;
